1f84add54fba1e769ceae62d2264bd24c5807d0b
limx <39614418+flymote@users.noreply.github.com>
2019-03-12 10:43:05 +0800

    Update FS_xmlcdr_list.php
    1、调整时间相关内容的显示，去掉冗余的日期显示；2、优化显示通话结束的原因和说明（原来仅显示原因码）；3、添加首页和尾页、最新几个链接，方便查看

--- FS_xmlcdr_list.php
+++ FS_xmlcdr_list.php
@@ -10,6 +10,8 @@ $conf = @unserialize(file_get_contents($file));
 //----------------------显示---------------------------------
 $_SESSION['POST_submit_once']=0;
 
+$Q850_label = array('UNSPECIFIED'=>'未指定','UNALLOCATED_NUMBER'=>'404 无此号','NO_ROUTE_TRANSIT_NET'=>'404 无法传输','NO_ROUTE_DESTINATION'=>'404 无路由','CHANNEL_UNACCEPTABLE'=>'频道不可接受','CALL_AWARDED_DELIVERED'=>'呼叫被授予','NORMAL_CLEARING'=>'正常清除','USER_BUSY'=>'486 用户忙','NO_USER_RESPONSE'=>'408 无响应','NO_ANSWER'=>' 480 未回答','SUBSCRIBER_ABSENT'=>'480 订户缺席','CALL_REJECTED'=>'603 呼叫被拒绝','NUMBER_CHANGED'=>'410 号码已变','REDIRECTION_TO_NEW_DESTINATION'=>'410 已重定向','EXCHANGE_ROUTING_ERROR'=>'483 路由错','DESTINATION_OUT_OF_ORDER'=>'502  目的地无序','INVALID_NUMBER_FORMAT'=>'484 无效格式','FACILITY_REJECTED'=>'501 设施被拒绝','RESPONSE_TO_STATUS_ENQUIRY'=>'状态的响应','NORMAL_UNSPECIFIED'=>'正常未定','NORMAL_CIRCUIT_CONGESTION'=>'503 无电路可用','NETWORK_OUT_OF_ORDER'=>'503 网络故障','NORMAL_TEMPORARY_FAILURE'=>'503 暂时失败','SWITCH_CONGESTION'=>'503 交换机拥塞','ACCESS_INFO_DISCARDED'=>'访问信息丢弃','REQUESTED_CHAN_UNAVAIL'=>'503 电路不可用','PRE_EMPTED'=>'预占位','RESOURCE_UNAVAILABLE'=>'资源不可用','FACILITY_NOT_SUBSCRIBED'=>'设施未订','OUTGOING_CALL_BARRED'=>'403 呼出被禁','INCOMING_CALL_BARRED'=>'403 来电被禁','BEARERCAPABILITY_NOTAUTH'=>'403 承载未授权','BEARERCAPABILITY_NOTAVAIL'=>'503 不可承载','SERVICE_UNAVAILABLE'=>'503 服务不可用','BEARERCAPABILITY_NOTIMPL'=>'488 承载未实现','CHAN_NOT_IMPLEMENTED'=>'通道未实现','FACILITY_NOT_IMPLEMENTED'=>'501 设施未实施','SERVICE_NOT_IMPLEMENTED'=>'501 服务未指定','INVALID_CALL_REFERENCE'=>'无效呼叫参考','INCOMPATIBLE_DESTINATION'=>'488 不兼容目的地','INVALID_MSG_UNSPECIFIED'=>'无效消息','MANDATORY_IE_MISSING'=>'缺失必要信息','MESSAGE_TYPE_NONEXIST'=>'消息不存在','WRONG_MESSAGE'=>'消息不兼容','IE_NONEXIST'=>'信息被丢弃','INVALID_IE_CONTENTS'=>'信息无效','WRONG_CALL_STATE'=>'呼叫不兼容','RECOVERY_ON_TIMER_EXPIRE'=>'504 定时到期恢复','MANDATORY_IE_LENGTH_ERROR'=>'参数不存在','PROTOCOL_ERROR'=>'协议错误','INTERWORKING'=>'互通未指定','ORIGINATOR_CANCEL'=>'487 请求被取消','CRASH'=>'崩溃','SYSTEM_SHUTDOWN'=>'系统关闭','LOSE_RACE'=>'进程中断','MANAGER_REQUEST'=>'命令挂断','BLIND_TRANSFER'=>'隐蔽的转移','ATTENDED_TRANSFER'=>'附加的转移','ALLOTTED_TIMEOUT'=>'指定的超时','USER_CHALLENGE'=>'用户质疑','MEDIA_TIMEOUT'=>'媒体超时','PICKED_OFF'=>'被分机截取','USER_NOT_REGISTERED'=>'用户未注册','PROGRESS_TIMEOUT'=>'处理超时','GATEWAY_DOWN'=>'网关关闭');
+$hangup_label = array("send_bye"=>"主叫挂机","recv_bye"=>"被叫挂机","send_refuse"=>"主叫拒绝","send_cancel"=>"主叫取消","recv_refuse"=>"被叫拒绝","recv_cancel"=>"被叫取消");
 echo "<html xmlns=http://www.w3.org/1999/xhtml><head><meta http-equiv=Content-Type content=\"text/html;charset=utf-8\">
 <link rel=\"stylesheet\" type=\"text/css\" href=\"main.css\"/>
 <style>
@@ -19,7 +21,7 @@ button{cursor:pointer;background:#f90;border:1px solid #303030;padding:3px;}
 h2{font-size:14pt;height:18pt;text-align:right;background:#FC0;border-bottom:3px solid #f90;padding:0px;margin:0px;}
 h2 span{color:#f90;cursor:pointer;background:#fff;border:1px solid #f90;padding:0 2px;}
 </style><script type=\"text/javascript\" src=\"jquery.js\"></script><script type=\"text/javascript\">
-function showcodeL(sid,iid){\$.post( \"FS_error_code.php\", { Q850_reason_label: sid }).done(function( data ) { $('#info'+iid).html(data);});}
+function showcodeL(sid,iid){\$.post( \"FS_error_code.php\", { Q850_reason_label: sid }).done(function( data ) { alert('*** 相关说明：\\n'+data);});}
 function showcodeC(sid,iid){\$.post( \"FS_error_code.php\", { Q850_reason_code: sid }).done(function( data ) { $('#info'+iid).html(data);});}
 function showcodeS(sid,iid){\$.post( \"FS_error_code.php\", { SIP_reason_code: sid }).done(function( data ) { $('#info'+iid).html(data);});}
 </script></head><body><div id=\"win\"><h2 id=\"tt\"><span id=\"close\" onclick=\"$('#win').css('display','none')\" style=\"pading:5px;\"> × </span></h2>
@@ -100,17 +102,29 @@ if (!empty($_GET['viewrelation'])){
 		$filename = $conf['CDR_file'];
 
 	while (($row = $result->fetch_array())!==false) {
-		if (!$row)
-			die('</table><p class=\'red\'><a href="?list=1&p='.($p-1<0?0:$p-1).$getstr.'">前一页</a> '.($p==0?1:$p+1).'  <a href="?p='.($p+1>$pages?$pages:$p+1).$getstr.'">下一页</a> 
+		if (!$row){
+			$pp = $p -1;
+			if ($pp < 0)
+				$first = '';
+			elseif($pp < 1)
+				$first = '<a href="?list=1&p=0'.$getstr.'">&laquo; 首页</a>';
+			else
+				$first = '<a href="?list=1&p=0'.$getstr.'">&laquo; 首页</a> <a href="?list=1&p='.($pp).$getstr.'">前一页</a>';
+			if ($p+1 >= $pages)
+				$end = '  <a href="?p='.($p).$getstr.'">尾页 &raquo;</a> ';
+			else
+				$end = '  <a href="?p='.($p+1).$getstr.'">下一页</a>  <a href="?p='.($pages-1).$getstr.'">尾页 &raquo;</a>';
+			die("</table><p class='red'>$first ".($p==0?1:$p+1).$end.'
     跳转到：<input id="topage" name="togape" value="" size=4><input type="submit" value="确认" onclick="pa = document.getElementById(\'topage\').value-1;
-    window.location.href=\'?p=\'+pa+\''.$getstr.'\';return false;"/></p></body></html>');
+    window.location.href=\'?p=\'+pa+\''.$getstr.'\';return false;"/> <a href="?">最新</a> </p></body></html>');
+		}
 		else{
 			if ($row['direction']=='inbound')
-				$shownumber = "<span class=\"bggreen\">来话inbound</span> $row[caller_id_number] ($row[caller_id_name]) -><span class=\"bold14\">$row[destination_number]</span>";
+				$shownumber = "<span class=\"bggreen\">来话</span> $row[caller_id_number] ($row[caller_id_name]) -><span class=\"bold14\">$row[destination_number]</span>";
 			else
-				$shownumber= "<span class=\"bgblue\">去话outbund</span> $row[caller_id_number] ($row[caller_id_name]) -><span class=\"bold14\">$row[destination_number]</span>";
+				$shownumber= "<span class=\"bgblue\">去话</span> $row[caller_id_number] ($row[caller_id_name]) -><span class=\"bold14\">$row[destination_number]</span>";
 			if ($row['read_codec'])
-				$showcode = " 读编码：<b>'$row[read_codec] $row[read_rate]'</b><br/> 写编码：<b> '$row[write_codec] $row[write_rate]'</b>";
+				$showcode = " 读码：<b>'$row[read_codec] $row[read_rate]'</b><br/> 写码：<b> '$row[write_codec] $row[write_rate]'</b>";
 			else 
 				$showcode = "<span class='smallgray smallsize-font'>无编码信息</span>";
 			if ($row['accountcode'])
@@ -134,7 +148,7 @@ if (!empty($_GET['viewrelation'])){
 			$showuser =" $accountcode $context $extension_uuid $sip_gateway $last_app ";
 			$filestr = "";
 			if (!empty($row['billsec'])){
-				$answertime =   "应答于 ".$row['answer_stamp']." 通话 $row[billsec] 秒  ($row[billmsec] 毫秒)";
+				$answertime =   "应答于 ".substr($row['answer_stamp'],11)." 通话 $row[billsec] 秒  ($row[billmsec] 毫秒)";
 				$pathstr = date("Ymd", $row['start_epoch']);
 				$namefind = $namerepl = array();
 				preg_match_all('|@([^@]*)@|',$filename,$nameparts);
@@ -152,12 +166,12 @@ if (!empty($_GET['viewrelation'])){
 				$filestr = @$_SESSION['recordings_dir'].'/'.$pathstr.'/'.$filestr0;
 				$fileplay = '/files/'.$pathstr.'/'.$filestr0;
 				if (file_exists($filestr))
-					$filestr = "<button type='button' onclick='$(\"#win\").css(\"display\",\"block\");$(\"#title\").html(\" <b>主叫：</b>$row[orig_caller_number] <b>被叫：</b>$row[orig_callee_number]\");$(\"#player\").attr(\"src\",\"$fileplay\");'> 【播放语音】 </button>";
+					$filestr = "<button type='button' onclick='$(\"#win\").css(\"display\",\"block\");$(\"#title\").html(\" <b>主叫：</b>$row[caller_id_number] <b>被叫：</b>$row[destination_number] \");$(\"#player\").attr(\"src\",\"$fileplay\");'> 【播放语音】 </button>";
 				else
 					$filestr = $fileplay = "";
 			}else
 				$answertime = "";
-			$showtime = "$row[start_stamp] 到 $row[end_stamp] <br/>$answertime 呼叫 $row[duration] 秒";
+			$showtime = $row['start_stamp'].' 到 '.substr($row['end_stamp'],11)." <br/>$answertime 呼叫 $row[duration] 秒";
 			if (!empty(($row["xml"]))){
 				$dtailinfo = "<span id='cdr-$row[uuid]' style='display:none;'>".urldecode(htmlentities(@gzuncompress ($row["xml"]),ENT_QUOTES))."</span>";
 			}else 
@@ -165,7 +179,7 @@ if (!empty($_GET['viewrelation'])){
 			$showip = "<span class='smallblack smallsize-font'>$showuser<br/><b>IP: </b>$row[ip]<br/><span onclick='\$(\"#cdr-$row[uuid]\").toggle()' style=\"cursor:pointer;\"> 【** 点击展开关闭 话单详情 **】-> </span>$dtailinfo</span>";
 			$bgcolor = fmod($i,2)>0?"class='bg1'":"class='bg2'";
 			$i++;
-			$showend = "<span class=\"smallgray smallsize-font\" style=\"cursor:pointer;\" onclick=\"showcodeL('$row[hangup_cause]',$i)\">$row[hangup_cause]</span> &nbsp; <span class=\"orange\" style=\"cursor:pointer;\"  onclick=\"showcodeC('$row[hangup_cause_q850]',$i)\">($row[hangup_cause_q850])</span> &nbsp; $row[sip_hangup_disposition]";
+			$showend = "<span class=\"smallgray smallsize-font\" style=\"cursor:pointer;\" onclick=\"showcodeL('$row[hangup_cause]',$i)\">".$Q850_label[$row['hangup_cause']]."</span> &nbsp;  ".$hangup_label[$row['sip_hangup_disposition']];
 			echo "<tr class=bg1><td>$shownumber $domain_name</td><td align=center>$showtime</td><td>$showcode</td><td>$showend  [$row[leg]] <br/>$filestr <span id='info$i' class=\"smallgray smallsize-font\"></span></td><td>";
 			if ($row['bridge_uuid'])
 				echo (isset($_GET['viewrelation'])?"<a href='?'>【查看全部】</a>":"<a href='?viewrelation=$row[uuid],$row[bridge_uuid]'>【关联查看】</a>");
